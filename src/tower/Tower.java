0ab8c711aed1c0f8330a813ab6d9c64d8c96bebf
jomoreno <jomoreno@PBIICO21202.ADLYON2.UNIV-LYON2.FR>
2017-11-17 11:07:19 +0100

    cc

--- src/tower/Tower.java
+++ src/tower/Tower.java
@@ -27,7 +27,7 @@ public class Tower extends Application {
             
             @Override
             public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
+                System.out.println("Hello!");
             }
         });
         
