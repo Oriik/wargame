113f38d3af38bfbeb50a869f2978b9db45ecd06f
Oriik <guillom.quiniou@hotmail.fr>
2017-11-18 20:42:16 +0100

    Add class Board & Player

--- src/tower/Tower.java
+++ src/tower/Tower.java
@@ -5,12 +5,21 @@
  */
 package tower;
 
+import java.awt.Event;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.ColumnConstraints;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 /**
@@ -21,22 +30,21 @@ public class Tower extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello!");
-            }
-        });
+    
+        Board board = new Board();
+               
+        Player player = new Player();
+        player.setRadius(5);
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        board.initBoard(player);
+                  
+        //On place le player à sa position initiale
+        StackPane temp = (StackPane)board.getChildren().get(0);
+        temp.getChildren().add(player);
+
+        Scene scene = new Scene(board, 300, 300);
         
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Tower");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -48,4 +56,8 @@ public class Tower extends Application {
         launch(args);
     }
     
+ 
+
+  
+   
 }
